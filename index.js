6a6432ffe91e12ac8e7d672a0beb3ea7e3ae4e11
Thanish <thanish@cinergix.com>
2018-12-19 12:30:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 var unescape = require('lodash.unescape');
+
 module.exports = function innerText(el, options) {
   options = options || {};
   var tags = options.tags || { p : '\n', br: '\n' };
   var html = el.innerHTML;
   html = html.replace(/&nbsp;/g, ' ');
   Object.keys( tags ).forEach(function(tag) {
-    // https://regex101.com/r/EtQdlD/3
-    html = html.replace( new RegExp( '<' + tag + '(>|.*?[^?]>)' , 'gi' ) , tags[tag] );
+    // NOTE: replace selected tags with new lines
+    //       https://regex101.com/r/Re5LGS/1
+    html = html.replace( new RegExp( `<${tag}[^>]*>` , 'gmi' ) , tags[tag] );
   });
-  html = html.replace(/<(\/|.)*?>/gi, '');
+  // NOTE: remove all closing tags and any other tags
+  //       https://regex101.com/r/UJrBXH/1
+  html = html.replace(/<\/?[^>]*>/gmi, '');
   return unescape( html );
 };
